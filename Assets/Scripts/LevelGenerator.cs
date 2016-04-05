5cf2680233dae971ccadfdec760234b370361b5b
DimitarSkripkin <m1taka2217@yahoo.com>
2016-04-05 14:50:22 +0300

    More or less stable version

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using Generator;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject endOfLevelTrigger;
+
+    public GameObject SpawnPoint;
+
+    public GameObject levelItemsHolder;
+
+    public GameObject levelWallsHolder;
+
+    public GameObject levelFloorsHolder;
+
+    public int roomSize = 10;
+
+    public int roomRowsCount = 4;
+
+    public int roomCollumnsCount = 4;
+
+    // TODO: rename?
+    public float roomItemsPadding = 0.2f;
+
+    public int seed = -1;
+
+    // Use this for initialization
+    void Start()
+    {
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Generate()
+    {
+        var items = levelItemsHolder
+            .GetComponentsInChildren<Transform>()
+            .Select(i => i.gameObject)
+            .Skip(1)
+            .ToArray();
+        //int itemsCount = items.Length;
+
+        var walls = levelWallsHolder
+            .GetComponentsInChildren<Transform>()
+            .Select(i => i.gameObject)
+            .Skip(1)
+            .ToArray();
+
+        var floors = levelFloorsHolder
+            .GetComponentsInChildren<Transform>()
+            .Select(i => i.gameObject)
+            .Skip(1)
+            .ToArray();
+
+        foreach (var item in items)
+        {
+            item.SetActive(false);
+        }
+
+        Vector2 seedOffset;
+        if (seed < 0)
+        {
+            seedOffset = new Vector2(Random.Range(0, 100), Random.Range(0, 100));
+            seed = Random.Range(0, 100);
+        }
+        else
+        {
+            seedOffset = new Vector2(seed, seed);
+        }
+
+        int gridCount = (1 << 3);
+        int dungeonSize = (1 << 2);
+
+        Vector2 startDungeonOffset = new Vector2(Random.Range(0, gridCount - dungeonSize), Random.Range(0, gridCount - dungeonSize));
+        //int dungeonSeed = seed % (gridCount - dungeonSize);
+        //startDungeonOffset = new Vector2(dungeonSeed, dungeonSeed);
+        Rect dungeonDimentions = new Rect(startDungeonOffset, new Vector2(dungeonSize, dungeonSize));
+
+        int populatedCells = 0;
+        RoomGenerator[] dungeonRooms = new RoomGenerator[dungeonSize * dungeonSize];
+        for (int cell = 0; cell < gridCount * gridCount; cell++)
+        {
+            int x, y;
+            d2xy(gridCount, cell, out x, out y);
+
+            // if cell is in the dungeon and the previous cell is neighbour add it to the list
+            if (dungeonDimentions.Contains(new Vector2(x, y)))
+            {
+                if (populatedCells > 0)
+                {
+                    var lastRoom = dungeonRooms[populatedCells - 1];
+                    if (Vector3.Distance(lastRoom.coord, new Vector3(x, 0.0f, y)) <= 1.0f)
+                    {
+                        var newRoom = new RoomGenerator();
+                        newRoom.Type = RoomType.Corridor;
+                        newRoom.coord.Set(x, 0.0f, y);
+
+                        if (lastRoom.coord.x < x)
+                        {
+                            lastRoom.doorsPositions |= (int)DoorPosition.Right;
+                            newRoom.doorsPositions = (int)DoorPosition.Left;
+                        }
+                        else if (lastRoom.coord.x > x)
+                        {
+                            lastRoom.doorsPositions |= (int)DoorPosition.Left;
+                            newRoom.doorsPositions = (int)DoorPosition.Right;
+                        }
+                        else if (lastRoom.coord.z < y)
+                        {
+                            lastRoom.doorsPositions |= (int)DoorPosition.Top;
+                            newRoom.doorsPositions = (int)DoorPosition.Bottom;
+                        }
+                        else if (lastRoom.coord.z > y)
+                        {
+                            lastRoom.doorsPositions |= (int)DoorPosition.Bottom;
+                            newRoom.doorsPositions = (int)DoorPosition.Top;
+                        }
+                        else
+                        {
+                            throw new UnityException("WTF?");
+                        }
+
+                        dungeonRooms[populatedCells] = newRoom;
+                        ++populatedCells;
+                    }
+                }
+                else
+                {
+                    dungeonRooms[populatedCells] = new RoomGenerator();
+                    dungeonRooms[populatedCells].coord.Set(x, 0.0f, y);
+                    dungeonRooms[populatedCells].Type = RoomType.Start;
+                    ++populatedCells;
+                }
+            }
+        }
+
+        dungeonRooms[populatedCells - 1].Type = RoomType.End;
+
+        int floorIndex = Random.Range(0, floors.Length);
+        for (int i = 0; i < populatedCells; ++i)
+        {
+            //float val = Mathf.PerlinNoise(seedOffset.x + dungeonRooms[i].coord.x / gridCount,
+            //    seedOffset.y + dungeonRooms[i].coord.z / gridCount);
+
+            //int floorIndex = Random.Range(0, floors.Length);
+            dungeonRooms[i].floorMaterial = floors[floorIndex]
+                     .GetComponent<MeshRenderer>()
+                     .sharedMaterial;
+
+            dungeonRooms[i].roomItems = items;
+            dungeonRooms[i].roomWalls = walls;
+            dungeonRooms[i].size = roomSize;
+            dungeonRooms[i].rowsCount = roomRowsCount;
+            dungeonRooms[i].collumnsCount = roomCollumnsCount;
+            dungeonRooms[i].roomItemsPadding = roomItemsPadding;
+            dungeonRooms[i].seed = seed;
+            dungeonRooms[i].Generate(this.gameObject, seedOffset);
+
+            GameObject specialItem;
+            if (i == populatedCells - 1)
+            {
+                specialItem = dungeonRooms[i].AddSpecialItem(endOfLevelTrigger, new Vector3(roomSize / 2.0f, 0.0f, roomSize / 2.0f));
+            }
+            else if (i == 0)
+            {
+                specialItem = dungeonRooms[i].AddSpecialItem(SpawnPoint, new Vector3(roomSize / 2.0f, 0.0f, roomSize / 2.0f));
+            }
+            else
+            {
+                specialItem = dungeonRooms[i].AddSpecialItem(SpawnPoint, new Vector3(roomSize / 2.0f, 0.0f, roomSize / 2.0f));
+            }
+
+            specialItem.SetActive(true);
+        }
+
+        //generated = true;
+    }
+
+    // taken from https://en.wikipedia.org/wiki/Hilbert_curve
+    //convert (x,y) to d
+    private int xy2d(int n, int x, int y)
+    {
+        int rx, ry, s, d = 0;
+        for (s = n / 2; s > 0; s /= 2)
+        {
+            rx = (x & s) > 0 ? 1 : 0;
+            ry = (y & s) > 0 ? 1 : 0;
+            d += s * s * ((3 * rx) ^ ry);
+            rot(s, ref x, ref y, rx, ry);
+        }
+        return d;
+    }
+
+    //convert d to (x,y)
+    private void d2xy(int n, int d, out int x, out int y)
+    {
+        int rx, ry, s, t = d;
+        x = y = 0;
+        for (s = 1; s < n; s *= 2)
+        {
+            rx = 1 & (t / 2);
+            ry = 1 & (t ^ rx);
+            rot(s, ref x, ref y, rx, ry);
+            x += s * rx;
+            y += s * ry;
+            t /= 4;
+        }
+    }
+
+    //rotate/flip a quadrant appropriately
+    private void rot(int n, ref int x, ref int y, int rx, int ry)
+    {
+        if (ry == 0)
+        {
+            if (rx == 1)
+            {
+                x = n - 1 - x;
+                y = n - 1 - y;
+            }
+
+            //Swap x and y
+            int t = x;
+            x = y;
+            y = t;
+        }
+    }
+}
---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,217 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Generator
+{
+    public enum RoomType
+    {
+        Start,
+        Corridor,
+        End,
+        //PosibleHiddenRoom// or bonus room
+    };
+
+    //[System.Flags]
+    public enum DoorPosition
+    {
+        Left = 1,
+        Right = 2,
+        Top = 4,
+        Bottom = 8
+    };
+
+    public class RoomGenerator
+    {
+        private GameObject floor;
+        
+        public Vector3 coord;
+
+        public RoomType Type;
+
+        public GameObject[] roomItems;
+
+        public GameObject[] roomWalls;
+
+        public Material floorMaterial;
+
+        public int size = 10;
+
+        public float wallHeight = 3.0f;
+
+        public int rowsCount = 4;
+
+        public int collumnsCount = 4;
+
+        // TODO: rename?
+        public float roomItemsPadding = 0.2f;
+
+        public int seed = -1;
+
+        // TODO: Rename!
+        //public DoorPosition doorsPositions;
+        public int doorsPositions = 0;
+
+        public void Generate(GameObject parent, Vector2 seedOffset)
+        {
+            floor = CreateSimplePlane(size, size, floorMaterial, "RoomFloor");
+            floor.transform.parent = parent.transform;
+            floor.transform.position = coord * size;
+
+            GameObject obj;
+            Vector3 position, direction;
+            Quaternion rotation;
+            switch (Type)
+            {
+                case RoomType.Start:
+                    // spawn player
+                    break;
+                case RoomType.Corridor:
+                    // spawn zombies?
+                    break;
+                case RoomType.End:
+                    // spawn end point
+                    break;
+                default:
+                    break;
+            }
+
+            Vector3 wallScale = new Vector3(size, wallHeight, 1.0f);
+            if (((int)doorsPositions & (int)DoorPosition.Top) == 0)
+            {
+                direction = new Vector3(0.0f, 0.0f, 1.0f);
+
+                rotation = Quaternion.LookRotation(direction, Vector3.up);
+                obj = (GameObject)Object.Instantiate(roomWalls[0], Vector2.zero, rotation);
+                obj.transform.localScale = wallScale;
+                obj.transform.parent = floor.transform;
+                position = (direction + new Vector3(1.0f, 0.0f, 1.0f)) * (size / 2);
+                obj.transform.localPosition = position;
+                obj.SetActive(true);
+            }
+
+            if (((int)doorsPositions & (int)DoorPosition.Bottom) == 0)
+            {
+                direction = new Vector3(0.0f, 0.0f, -1.0f);
+
+                rotation = Quaternion.LookRotation(direction, Vector3.up);
+                obj = (GameObject)Object.Instantiate(roomWalls[0], Vector2.zero, rotation);
+                obj.transform.localScale = wallScale;
+                obj.transform.parent = floor.transform;
+                position = (direction + new Vector3(1.0f, 0.0f, 1.0f)) * (size / 2);
+                obj.transform.localPosition = position;
+                obj.SetActive(true);
+            }
+
+            if (((int)doorsPositions & (int)DoorPosition.Left) == 0)
+            {
+                direction = new Vector3(-1.0f, 0.0f, 0.0f);
+
+                rotation = Quaternion.LookRotation(direction, Vector3.up);
+                obj = (GameObject)Object.Instantiate(roomWalls[0], Vector2.zero, rotation);
+                obj.transform.localScale = wallScale;
+                obj.transform.parent = floor.transform;
+                position = (direction + new Vector3(1.0f, 0.0f, 1.0f)) * (size / 2);
+                obj.transform.localPosition = position;
+                obj.SetActive(true);
+            }
+
+            if (((int)doorsPositions & (int)DoorPosition.Right) == 0)
+            {
+                direction = new Vector3(1.0f, 0.0f, 0.0f);
+
+                rotation = Quaternion.LookRotation(direction, Vector3.up);
+                obj = (GameObject)Object.Instantiate(roomWalls[0], Vector2.zero, rotation);
+                obj.transform.localScale = wallScale;
+                obj.transform.parent = floor.transform;
+                position = (direction + new Vector3(1.0f, 0.0f, 1.0f)) * (size / 2);
+                obj.transform.localPosition = position;
+                obj.SetActive(true);
+            }
+
+            //*
+            float cellSizeX = size / (float)collumnsCount;
+            float cellSizeY = size / (float)rowsCount;
+
+            for (int m = 0; m < rowsCount; m++)
+            {
+                float yMin = cellSizeY * m;
+                float yMax = cellSizeY * (m + 1);
+
+                yMin += cellSizeY * roomItemsPadding;
+                yMax -= cellSizeY * roomItemsPadding;
+                for (int n = 0; n < collumnsCount; n++)
+                {
+                    float xMin = cellSizeX * n;
+                    float xMax = cellSizeX * (n + 1);
+
+                    xMin += cellSizeX * roomItemsPadding;
+                    xMax -= cellSizeX * roomItemsPadding;
+
+                    float x = Random.Range(xMin, xMax);
+                    float y = Random.Range(yMin, yMax);
+                    float val = Mathf.PerlinNoise(seedOffset.x + x, seedOffset.y + y);
+
+                    if (val >= 0.0f && val < 1.0f)
+                    {
+                        var item = roomItems[(int)(val * roomItems.Length)];
+                        obj = (GameObject)Object.Instantiate(item, Vector2.zero, Quaternion.identity);
+                        obj.transform.parent = floor.transform;
+                        position = new Vector3(x, 0.0f, y);
+                        obj.transform.localPosition = position;
+                        obj.SetActive(true);
+                    }
+                }
+            }//*/
+        }
+
+        public GameObject AddSpecialItem(GameObject item, Vector3 roomPosition)
+        {
+            var newObj = (GameObject)Object.Instantiate(item, Vector3.zero, Quaternion.identity);
+            newObj.transform.parent = floor.transform;
+            newObj.transform.localPosition = roomPosition;
+            return newObj;
+        }
+
+        private GameObject CreateSimplePlane(float width, float height, Material material, string name)
+        {
+            //var plane = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            //plane.name = name;
+            // how to scale it????
+            //return plane;
+            Mesh mesh = new Mesh()
+            {
+                name = "ScriptedPlane",
+                vertices = new Vector3[]
+                {
+                    new Vector3(0.0f, 0.0f, 0.0f),
+                    new Vector3(width, 0.0f, 0.0f),
+                    new Vector3(width, 0.0f, height),
+                    new Vector3(0.0f, 0.0f, height)
+                },
+                uv = new Vector2[]
+                {
+                    new Vector2 (0, 0),
+                    new Vector2 (0, 1),
+                    new Vector2(1, 1),
+                    new Vector2 (1, 0)
+                },
+                triangles = new int[]
+                {
+                    0, 2, 1,
+                    0, 3, 2
+                }
+            };
+            mesh.RecalculateNormals();
+
+            GameObject plane = new GameObject(name);
+            MeshFilter meshFilter = plane.AddComponent<MeshFilter>();
+            meshFilter.sharedMesh = mesh;
+            MeshCollider meshCollider = plane.AddComponent<MeshCollider>();
+            meshCollider.sharedMesh = mesh;
+            MeshRenderer meshRenderer = plane.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = material;
+
+            return plane;
+        }
+    }
+}
